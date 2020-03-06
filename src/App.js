0c7164c17ebec0c2e9d8f5bd1dd971365e1489e1
John William Davis <jwdavisdeveloper@gmail.com>
2020-03-06 10:42:17 -0800

    changes after code-along with anna

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import FilmListing from './FilmListing';
 import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
 
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      films: TMDB.films,
-      current: {}
-    }
+function App() {
+  const [films, setFilms] = useState(TMDB.films);
+  const [current, setCurrent] = useState({});
+
+  
+
+ let handleDetailsClick = (film) => {
+    const url = `https//api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`;
+    fetch(url)
+    .then(response => response.json())
+    .then(json => setCurrent(json))
+    .catch(err => console.log(`Error: ${err}`));
+    
+
   }
 
-  handleDetailsClick = (film) => {
-    console.log("Fetching details for", film.title);
-  }
-
-  render() {
+  
     return (
       <div className="App">
         <div className="film-library">
-          <FilmListing films={this.state.films} 
-          handleDetailsClick={this.handleDetailsClick}
+          <FilmListing films={films} 
+          handleDetailsClick={handleDetailsClick}
           />
-          <FilmDetails film={this.state.current}/>
+          <FilmDetails film={current}/>
         </div>
       </div>
     );
-  }
+  
 }
 
 export default App;
--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilmDetails extends Component {
-  render() {
+function FilmDetails(props) {
+    const backdropURL = `https://image.tmdb.org/t/p/w1280/${props.film.backdrop_path}`;
+    const posterURL = `https://image.tmdb.org/t/p/w780/${props.film.poster_path}`
     return (
+
           <div className="film-details">
             <h1 className="section-title">DETAILS</h1>
+          <div className="film-backdrop">
+          <img src={backdropURL} />
+          </div>
+          <div className="film-meta">
+            <h2 className="film-tagline">{props.film.tagline}</h2>
+            <p className="film-detail-overview">
+            <img src={posterURL} className="film-detail_poster" />
+            {props.film.overview}
+            </p>
+          </div>
+
           </div>
     );
+    
   }
-}
+
 
 export default FilmDetails;
